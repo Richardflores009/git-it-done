0bdb3d2bad392e00a2c0546230bf6da479ab2909
Richy Rich <richardflores009@gmail.com>
2020-09-01 20:51:27 -0700

    added link that takes you to github if more than 3 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning")
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,12 +8,10 @@ var getRepoIssues = function (repo) {
         // request was successful
         if (response.ok) {
             response.json().then(function (data) {
-                // pass response data to dom function
-                displayIssues(data)
+                displayIssues(data);
+
+                displayWarning(repo)
             });
-        }
-        else {
-            alert("There was a problem with your request!");
         }
     });
 };
@@ -56,4 +54,16 @@ var displayIssues = function (issues) {
 
         issueContainerEl.appendChild(issueEl);
     }
+}
+
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textcontent = "to see more than 30 issues, visit";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com"
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 }
